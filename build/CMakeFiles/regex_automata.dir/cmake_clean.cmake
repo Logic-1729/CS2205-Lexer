43d5a7d35a62fc3f6fcde3bfc9d486f5c5852a21
Logic-1729 <logic_1729@sjtu.edu.cn>
2025-11-26 11:35:40 +0800

    fix: UPDATE THE SHELL

---
+++ build/CMakeFiles/regex_automata.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/regex_automata.dir/src/dfa_converter.cpp.o"
+  "CMakeFiles/regex_automata.dir/src/dfa_converter.cpp.o.d"
+  "CMakeFiles/regex_automata.dir/src/dfa_minimizer.cpp.o"
+  "CMakeFiles/regex_automata.dir/src/dfa_minimizer.cpp.o.d"
+  "CMakeFiles/regex_automata.dir/src/infix_to_postfix.cpp.o"
+  "CMakeFiles/regex_automata.dir/src/infix_to_postfix.cpp.o.d"
+  "CMakeFiles/regex_automata.dir/src/main.cpp.o"
+  "CMakeFiles/regex_automata.dir/src/main.cpp.o.d"
+  "CMakeFiles/regex_automata.dir/src/nfa_builder.cpp.o"
+  "CMakeFiles/regex_automata.dir/src/nfa_builder.cpp.o.d"
+  "CMakeFiles/regex_automata.dir/src/regex_preprocessor.cpp.o"
+  "CMakeFiles/regex_automata.dir/src/regex_preprocessor.cpp.o.d"
+  "CMakeFiles/regex_automata.dir/src/visualize.cpp.o"
+  "CMakeFiles/regex_automata.dir/src/visualize.cpp.o.d"
+  "regex_automata"
+  "regex_automata.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/regex_automata.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
